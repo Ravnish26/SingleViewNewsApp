c7460ab9b20cbe824d16fdef987701d996180777
RavnishSignh_26 <65758722+Ravnish26@users.noreply.github.com>
2022-12-30 12:15:30 +0530

    Add files via upload

---
+++ NewsAppApi/NewsAppApi/NewsAppApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApiApp.swift
+//  NewsAppApi
+//
+//  Created by Ravnish Singh on 27/12/22.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
